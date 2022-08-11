378a59030c2d249de80e9f0b62a1411d4d78b60a
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-11 22:48:07 +0300

    set login states

---
+++ src/pages/Login.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+
+function Login() {
+
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+  
+  const [user, setUser] = useState({name:"", email:"", password:""})"});
+  const [error, setError] = useState("");
+
+
+  const Login = details => {
+    console.log(details);
+  }
+
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
