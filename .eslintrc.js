a781baebd51e521d32ecac2f0b418dab7691fff9
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-15 05:17:27 +0300

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
+    "prettier/prettier": "warn",
     'max-len': [
       2,
       550,
